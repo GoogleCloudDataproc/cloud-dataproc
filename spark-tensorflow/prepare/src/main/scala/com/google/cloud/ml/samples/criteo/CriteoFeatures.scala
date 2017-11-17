9839d2bb506c5a41905b8079c6a512f455568c86
Bill Prin <waprin@google.com>
2017-11-17 14:54:49 -0800

    Style fixes

--- spark-tensorflow/prepare/src/main/scala/com/google/cloud/ml/samples/criteo/CriteoFeatures.scala
+++ spark-tensorflow/prepare/src/main/scala/com/google/cloud/ml/samples/criteo/CriteoFeatures.scala
@@ -29,7 +29,6 @@ case class CriteoFeatures() {
   val clickedLabel = Seq("clicked")
 
   val inputLabels: Seq[String] = clickedLabel ++ integerFeatureLabels ++ categoricalRawLabels
-  //val inputLabels: Seq[String] = clickedLabel ++ integerFeatureLabels ++ categoricalRawLabels
 
   val integralColumns: Seq[String] = inputLabels.
     filterNot(label => categoricalRawLabels.contains(label))
