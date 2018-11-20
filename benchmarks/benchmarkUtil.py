c833f682ef503487cbb0c0f3b4a2461c058129fe
Aniket Mokashi <aniket486@gmail.com>
2018-11-19 17:51:53 -0800

    Fix merging logic and templates under benchmarks logic to handle template syntax changes

--- benchmarks/benchmarkUtil.py
+++ benchmarks/benchmarkUtil.py
@@ -12,6 +12,7 @@ class Scenario:
 
 
 def execute_shell(cmd):
+    print("Running command {}".format(cmd))
     p = subprocess.Popen(
         cmd,
         shell=True,
@@ -30,11 +31,12 @@ class Benchmark:
     scenarios = []
 
     def run_workflow(self, scenario):
-        command = "gcloud beta dataproc workflow-templates instantiate-from-file --file {} --format json".format(
+        command = "gcloud dataproc workflow-templates instantiate-from-file --file {} --format json".format(
             scenario.config_file_name)
         stdout, err, return_code = execute_shell(command)
         if return_code != 0:
             print("Workflow execution failed - Error code is {}".format(return_code))
+            print("ERROR:\n {}\n".format(err))
             yaml_config = yaml.safe_load(open(scenario.config_file_name, 'r'))
             prefix = yaml_config['placement']['managedCluster']['clusterName']
             pattern = re.compile(r"{}-.*.".format(prefix))
@@ -79,36 +81,42 @@ class Benchmark:
         stream.close()
         return template
 
-    def write_scenarios_yaml(self, data, scenario):
-        stream = open("{}-{}".format(scenario, "cfg.yaml"), 'w')
+    def write_scenarios_yaml(self, data, scenario, scenario_file):
+        stream = open(scenario_file, 'w')
         yaml.dump(data, stream, default_flow_style=False)
         stream.close()
 
-    def merge_dicts(self, d1, d2):
+    def merge_dicts(self, original, override):
         """
-        Recursively overrides items in d1 based on keys/values from d2.
+        Recursively overrides items in original based on keys/values from override.
         Input dictionaries must have common scheme.
         """
-        for override_key, override_item in d2.items():
-            if override_key in d1:
+        for override_key, override_item in override.items():
+            if override_key in original:
                 if isinstance(override_item, dict):
-                    self.merge_dicts(d1[override_key], override_item)
+                    self.merge_dicts(original[override_key], override_item)
                 elif isinstance(override_item, list):
-                    d1[override_key] = [i[1] for i in zip(d1[override_key], override_item)]
+                    original_list = original[override_key]
+                    for idx, item in enumerate(override_item):
+                        if isinstance(item, dict):
+                            self.merge_dicts(original_list[idx], item)
+                        else:
+                            original_list[idx] = item
                 else:
-                    d1[override_key] = override_item
+                    original[override_key] = override_item
             else:
-                d1[override_key] = override_item
+                original[override_key] = override_item
 
     def merge_configs(self):
         """Apply config from scenario to template."""
-        scenarios_file = self.read_scenarios_yaml()
-        for scenario in scenarios_file:
+        scenarios = self.read_scenarios_yaml()
+        for scenario in scenarios:
             base_config = self.read_template_yaml()
             # Iterate over scenarios_file
-            scenario_dict = scenarios_file[scenario]
+            scenario_dict = scenarios[scenario]
             self.merge_dicts(base_config, scenario_dict)
-            if os.path.exists("{}-{}".format(scenario, "cfg.yaml")):
-                print("{}-{}".format(scenario, "cfg.yaml") + " already exists will be overwritten.")
-            self.write_scenarios_yaml(base_config, scenario)
-            self.scenarios.append(Scenario(scenario, "{}-{}".format(scenario, "cfg.yaml")))
+            scenario_file = "{}/{}-{}".format("/tmp",scenario, "cfg.yaml")
+            if os.path.exists(scenario_file):
+                print(scenario_file + " already exists will be overwritten.")
+            self.write_scenarios_yaml(base_config, scenario, scenario_file)
+            self.scenarios.append(Scenario(scenario, scenario_file))
