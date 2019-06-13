d63c2056c3d7d28070592a4c13b4b343d6e45888
Dagang Wei <functicons@gmail.com>
2019-06-12 18:23:44 -0700

    Fix test

--- custom-images/test_shell_script_generator.py
+++ custom-images/test_shell_script_generator.py
@@ -8,26 +8,32 @@ _expected_script = """
 
 set -euxo pipefail
 
-disk_created="false"
-vm_created="false"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 function exit_handler() {
   echo 'Cleaning up before exiting.'
+  exit_code=$?
 
-  if [[ "$vm_created" == "true" ]]; then
+  if [[ -f /tmp/custom-image-my-image-20190611-160823/vm_created ]]; then
     echo 'Deleting VM instance.'
     gcloud compute instances delete my-image-install         --project=my-project --zone=us-west1-a -q
-  fi
-
-  if [[ "$disk_created" == "true" && "$vm_created" == "false" ]]; then
+  elif [[ -f /tmp/custom-image-my-image-20190611-160823/disk_created ]]; then
     echo 'Deleting disk.'
     gcloud compute disks delete my-image-install --project=my-project --zone=us-west1-a -q
   fi
 
   echo 'Uploading local logs to GCS bucket.'
-  gsutil -m rsync -r /tmp/custom-image-my-image-20190611-160823/logs gs://my-bucket/custom-image-my-image-20190611-160823/logs/
+  gsutil -m rsync -r /tmp/custom-image-my-image-20190611-160823/logs/ gs://my-bucket/custom-image-my-image-20190611-160823/logs/
 
-  sleep 5
+  if [[ $exit_code -eq 0 ]]; then
+    echo -e "${GREEN}Workflow succeeded${NC}"
+    echo -e "${GREEN}Check logs at /tmp/custom-image-my-image-20190611-160823/logs/ or gs://my-bucket/custom-image-my-image-20190611-160823/logs/"
+  else
+    echo -e "${RED}Workflow failed${NC}."
+    echo -e "${RED}Check logs at /tmp/custom-image-my-image-20190611-160823/logs/ or gs://my-bucket/custom-image-my-image-20190611-160823/logs/"
+  fi
 }
 
 function main() {
@@ -37,21 +43,21 @@ function main() {
 
   echo 'Creating disk.'
   gcloud compute disks create my-image-install       --project=my-project       --zone=us-west1-a       --image=projects/cloud-dataproc/global/images/dataproc-1-4-deb9-20190510-000000-rc01       --type=pd-ssd       --size=40GB
-  disk_created="true"
+  touch /tmp/custom-image-my-image-20190611-160823/disk_created
 
   echo 'Creating VM instance to run customization script.'
   gcloud compute instances create my-image-install       --project=my-project       --zone=us-west1-a  --subnet=my-subnet       --machine-type=n1-standard-2       --disk=auto-delete=yes,boot=yes,mode=rw,name=my-image-install       --scopes=cloud-platform       --metadata=shutdown-timer-in-sec=500,daisy-sources-path=gs://my-bucket/custom-image-my-image-20190611-160823/sources,startup-script-url=gs://my-bucket/custom-image-my-image-20190611-160823/sources/run.sh
-  vm_created="true"
+  touch /tmp/custom-image-my-image-20190611-160823/vm_created
 
-  echo 'Waiting for VM instance to shutdown.'
+  echo 'Waiting for customization script to finish.'
   gcloud compute instances tail-serial-port-output my-image-install       --project=my-project       --zone=us-west1-a       --port=1 2>&1       | grep 'startup-script'       | tee /tmp/custom-image-my-image-20190611-160823/logs/startup-script.log       || true
 
   echo 'Checking customization script result.'
   if grep 'BuildFailed:' /tmp/custom-image-my-image-20190611-160823/logs/startup-script.log; then
-    echo 'Customization script failed.'
+    echo -e "${RED}Customization script failed.${NC}"
     exit 1
   elif grep 'BuildSucceeded:' /tmp/custom-image-my-image-20190611-160823/logs/startup-script.log; then
-    echo 'Customization script succeeded.'
+    echo -e "${GREEN}Customization script succeeded.${NC}"
   else
     echo 'Unable to determine whether customization script result.'
     exit 1
